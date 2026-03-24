0834434ead061c488fa630dcff7dce302f09227d
YARLAGADDA NEHAPRIYA <nehapriya.yIC@gmail.com>
2026-03-24 16:15:28 +0530

    Add files via upload

---
+++ HotelBookingAPi/DTOs/HotelDto.cs
@@ -0,0 +1,24 @@
+﻿namespace HotelBookingAPi.DTOs
+{
+    public class HotelDto
+    {
+        public int HotelId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string Amenities { get; set; } = string.Empty;
+    }
+
+    public class CreateHotelDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string Amenities { get; set; } = string.Empty;
+    }
+
+    public class UpdateHotelDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string Amenities { get; set; } = string.Empty;
+    }
+}
