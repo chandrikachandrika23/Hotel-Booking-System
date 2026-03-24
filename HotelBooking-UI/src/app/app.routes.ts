b3bd445829ecd1d32572489f551fd05a638a57c5
LohithaSammagi <lohithasammangi12@gmail.com>
2026-03-24 18:46:05 +0530

    Delete HotelBooking-UI/src/app/app.routes.ts

--- HotelBooking-UI/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { HotelsListComponent } from './hotels/hotels-list/hotels-list.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'hotels', pathMatch: 'full' },
-  { path: 'hotels', component: HotelsListComponent },
-];
