767afe3abbf26bb328be0c0a1baa726abb6dcb05
Nehapriya30 <nehapriya.yic@gmail.com>
2026-03-24 15:24:11 +0530

    feat: added hotels module - model, DTO, controller

---
+++ HotelBooking-UI/src/app/hotels/hotels-list/hotels-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hotels-list',
+  imports: [],
+  templateUrl: './hotels-list.html',
+  styleUrl: './hotels-list.css',
+})
+export class HotelsList {
+
+}
