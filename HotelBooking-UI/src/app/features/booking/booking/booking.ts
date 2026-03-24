8ad0b8fd2750e10e5d143457fd6940993f831fc4
Chandrika Rajulapati <chandrikarajulapati317@gmail.com>
2026-03-24 18:58:06 +0530

    Define Booking, Room, and Hotel interfaces
    Added Booking, Room, and Hotel interfaces to define data structures for the booking feature.

--- HotelBooking-UI/src/app/features/booking/booking/booking.ts
+++ HotelBooking-UI/src/app/features/booking/booking/booking.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+export interface Booking {
+  bookingId: number;
+  userId: number;
+  roomId: number;
 
-@Component({
-  selector: 'app-booking',
-  imports: [],
-  templateUrl: './booking.html',
-  styleUrl: './booking.css',
-})
-export class Booking {}
+  checkInDate: Date;
+  checkOutDate: Date;
+
+  totalAmount: number;
+  status: string;
+
+  // 🔹 Navigation properties (from backend Include)
+  room?: Room;
+}
+
+export interface Room {
+  roomId: number;
+  roomNumber: string;
+  price: number;
+
+  hotel?: Hotel;
+}
+
+export interface Hotel {
+  hotelId: number;
+  name: string;
+  location: string;
+}
